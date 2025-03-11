74b7cda2c8f7e1719a3afda3566d736d025afca2
Dino0204 <s24020@gsm.hs.kr>
2025-03-11 19:45:39 +0900

    🎨 Improve: 환경 변수 한 파일에서 관리

---
+++ src/apis/env_valuable.ts
@@ -0,0 +1,5 @@
+export const KR = import.meta.env.VITE_RIOT_KR_URL;
+export const ASIA = import.meta.env.VITE_RIOT_ASIA_URL;
+export const RIOT_API = import.meta.env.VITE_RIOT_API;
+export const DDRAGON = import.meta.env.VITE_DDRAGON_URL;
+export const VERSION = import.meta.env.VITE_DDRAGON_VER;
